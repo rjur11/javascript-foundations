eed1d465e29cb516d454d27320bb3dc7e1785c0d
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-10-18 21:51:08 -0400

    Finish crafts and materials

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor(typeObj) {
+    this.name = typeObj.type;
+    this.materials = typeObj.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return "All done! It looks great!";
+  }
+
+  calculateProjectTotal() {
+    var totalPrice = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      totalPrice += this.materials[i].calculateMaterialCost();
+    }
+    return totalPrice;
+  }
 }
 
 module.exports = Craft;
