59eae79d34be3adff1de8aa95c388374f0a0e58f
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-10-27 12:24:28 -0400

    Finish snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,19 @@
-var Snowman = require('./Snowman');
+var Snowman = require("./Snowman");
 
 class Human {
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {snowballs: 0, coal: 0, buttons: 0, carrots: 0};
+  }
 
+  gatherMaterials(material, amount) {
+    this.materials[material] += amount;
+  }
+
+  buildASnowman(snowmanPieces) {
+    var newSnowman = new Snowman(snowmanPieces);
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,24 @@
 class Snowman {
+  constructor(snowmanPieces) {
+    this.carrots = snowmanPieces.carrots;
+    this.coal = snowmanPieces.coal;
+    this.buttons = snowmanPieces.buttons;
+    this.snowballs = snowmanPieces.snowballs;
+    this.magicHat = false;
+  }
 
+  canWearMagicHat() {
+    if (
+      this.coal < 2 ||
+      this.buttons < 5 ||
+      this.carrots < 1 ||
+      this.snowballs < 2
+    ) {
+      return false;
+    } else {
+      this.magicHat = true;
+    }
+  }
 }
 
 module.exports = Snowman;
